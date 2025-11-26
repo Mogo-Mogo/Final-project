34f4f0fa88c269fcc2a965e7c722a7aa5db7d39a
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-26 17:38:33 +0000

    --- Auto Save ---

--- project/FlashcardsBackend/index.js
+++ project/FlashcardsBackend/index.js
@@ -2,10 +2,10 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 async function connectDB() {
-try {
-await mongoose.connect(process.env.MONGO_URI);
-console.log('Connected to MongoDB');
-} catch (err) {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to MongoDB');
+    } catch (err) {
         console.error('Mongo connection error:', err);
     }
 }
