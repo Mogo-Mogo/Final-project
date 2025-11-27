22a268d0de7e88a927a3105659e936fbc8914ef8
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-27 16:23:09 +0000

    --- Auto Save ---

---
+++ project/FlashcardsBackend/routes/decksRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+  createCard,
+  getAllCards,
+  getCardById,
+  deleteCard,
+} from '../controllers/cardsController.js';
+
+const router = express.Router();
+
+router.get('/', getAllCards);
+router.get('/:id', getCardById);
+router.post('/', createCard);
+router.delete('/:id', deleteCard);
+
+export default router;
