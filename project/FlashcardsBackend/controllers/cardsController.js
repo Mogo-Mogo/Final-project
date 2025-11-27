fe3bb643552df795c9e6f6eb3083f543fe52b690
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-27 17:30:11 +0000

    --- Auto Save ---

--- project/FlashcardsBackend/controllers/cardsController.js
+++ project/FlashcardsBackend/controllers/cardsController.js
@@ -67,7 +67,7 @@ export const updateCard = async (req, res) => {
         const updated = await Card.findByIdAndUpdate(
             id,
             { $set: req.body },
-            { new: true}
+            { new: true, runValidators: true }
         );
 
         if (!updated) {
