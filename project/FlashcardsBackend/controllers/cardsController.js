d5c632730338da8b6922aadac47f6797d79322e4
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-27 17:19:25 +0000

    --- Auto Save ---

--- project/FlashcardsBackend/controllers/cardsController.js
+++ project/FlashcardsBackend/controllers/cardsController.js
@@ -45,7 +45,7 @@ export const getCardById = async (req, res) => {
 
 export const getCardsByDeck = async (req, res) => {
     try {
-        const { deckId } = req.params;
+        const { deckId } = req.params.deckId;
         if (!mongoose.Types.ObjectId.isValid(deckId)) {
             return res.status(400).json({ error: 'Invalid deck id' });
         }
