82f7400f144753bfaa1c8755fce6dff6eb34310c
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 21:42:24 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashcardsTrainer/app/(tabs)/_layout.tsx
@@ -2,14 +2,23 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-        <Tabs.Screen
-        name="(decks)"
+     <Tabs>
+      <Tabs.Screen
+        // Name of the dynamic route.
+        name="[user]"
         options={{
-            tabBarLabel: "Decks",
-            title: "Decks"
+          // Ensure the tab always links to the same href.
+          href: '/evanbacon',
+          // OR you can use the href object.
+          href: {
+            pathname: '/[user]',
+            params: {
+              user: 'evanbacon',
+            },
+          },
         }}
       />
+    </Tabs>
     </Tabs>
   );
 }
