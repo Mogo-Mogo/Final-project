744b8b6636878a065eabf95b7e87e7133c718077
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 20:11:49 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashcardsTrainer/app/(tabs)/_layout.tsx
@@ -4,9 +4,9 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-    <Tabs.Screen
-        name="home"
-        options={{
+        <Tabs.Screen
+          name="home"
+            options={{
           tabBarLabel: "Home",
           title: "Home"
         }}
