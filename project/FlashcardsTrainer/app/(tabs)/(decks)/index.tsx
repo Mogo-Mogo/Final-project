841a14d380536ae02590054b0abfeee56df5f395
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 20:15:04 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/(decks)/index.tsx
+++ project/FlashcardsTrainer/app/(tabs)/(decks)/index.tsx
@@ -1,6 +1,12 @@
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Tab() {
+import { Text } from 'react-native';
+
+export default function Page() {
+  return <Text>Index page of Home Tab</Text>;
+}
+
+export function Tab() {
   return (
     <View style={styles.container}>
       <Text>Tab [Home|Settings]</Text>
