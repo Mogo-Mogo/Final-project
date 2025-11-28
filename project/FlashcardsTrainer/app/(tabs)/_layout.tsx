aaad76ea379c533fc777106ebd5a307e47459a71
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 22:07:10 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashcardsTrainer/app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from 'expo-router';
 export default function DecksLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="decks" options={{ title: 'Decks' }} />
+      <Tabs.Screen name="decks" options={{ title: 'decks' }} />
       <Tabs.Screen name="settings" options={{ title: 'Study' }} />
     </Tabs>
   );
