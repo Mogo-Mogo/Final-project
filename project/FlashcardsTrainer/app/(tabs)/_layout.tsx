e25ae94237748e4df614bbf4db9463af02eb48d6
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-11-28 20:12:04 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/_layout.tsx
+++ project/FlashcardsTrainer/app/(tabs)/_layout.tsx
@@ -5,10 +5,10 @@ export default function TabLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
         <Tabs.Screen
-          name="home"
-            options={{
-          tabBarLabel: "Home",
-          title: "Home"
+        name="home"
+        options={{
+            tabBarLabel: "Home",
+            title: "Home"
         }}
       />
     </Tabs>
