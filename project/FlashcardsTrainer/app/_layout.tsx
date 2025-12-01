5dc1eca207bafefbc458ceb64a21a4d80626a9fa
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 22:14:23 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/_layout.tsx
+++ project/FlashcardsTrainer/app/_layout.tsx
@@ -2,21 +2,22 @@ import { Stack, Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <view
-    <Stack>
-      <Stack.Screen
-        name="(modals)/create-deck"
-        options={{ presentation: "modal", title: "New Deck" }}
-      />
-      <Stack.Screen
-        name="(modals)/create-card"
-        options={{ presentation: "modal", title: "New Card" }}
-      />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-    <Tabs>
-      <Tabs.Screen name="decks" options={{ title: 'decks'}} />
-      <Tabs.Screen name="study" options={{ title: 'study'}} />
-    </Tabs>
+    <view>
+      <Stack>
+        <Stack.Screen
+          name="(modals)/create-deck"
+          options={{ presentation: "modal", title: "New Deck" }}
+        />
+        <Stack.Screen
+          name="(modals)/create-card"
+          options={{ presentation: "modal", title: "New Card" }}
+        />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <Tabs>
+        <Tabs.Screen name="decks" options={{ title: 'decks'}} />
+        <Tabs.Screen name="study" options={{ title: 'study'}} />
+      </Tabs>
+    </view>
   );
 }
