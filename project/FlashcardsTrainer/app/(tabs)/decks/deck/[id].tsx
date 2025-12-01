ab9e6d01284e6f524e5a36eae8d6131c151c7298
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 21:57:37 +0000

    --- Auto Save ---

---
+++ project/FlashcardsTrainer/app/(tabs)/decks/deck/[id].tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function IdPage() {
+  return (
+    <View style={styles.container}>
+      <Text>this is the decks.index page for some reason</Text>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
