49d99bf9ccea4d7952c24156177694bafb97b5a0
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 23:22:40 +0000

    --- Auto Save ---

--- project/FlashcardsBackend/index.js
+++ project/FlashcardsBackend/index.js
@@ -7,7 +7,10 @@ import cardsRoutes from './routes/cardsRoutes.js';
 import decksRoutes from './routes/decksRoutes.js'; 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://ubiquitous-journey-wrrp96vxrggpfgjqg-8081.app.github.dev', 'http://localhost:8081'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
