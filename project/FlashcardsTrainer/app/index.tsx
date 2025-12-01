4711e0433e0a6d56d6554f46bf2f9ab2d281170d
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 22:30:53 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/index.tsx
+++ project/FlashcardsTrainer/app/index.tsx
@@ -2,7 +2,7 @@
 import { View, Text } from 'react-native';
 
 
-export default function Page() {
+export default function App() {
   return <View>
     <Text>Welcome to Flashcards Trainer!</Text>
   </View>
