c4ae94f5d8b05adb0636973f57a3c1937daa7d6c
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 22:24:53 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/index.tsx
+++ project/FlashcardsTrainer/app/index.tsx
@@ -1,19 +1,5 @@
-import { useEffect } from 'react';
-import { useRouter } from 'expo-router';
-import { View } from 'react-native';
-
-export default function Page() {
-  const router = useRouter();
-  
-  useEffect(() => {
-    router.replace('/(tabs)/study');
-  }, []);
-
-  return <View />;
-}
-
-/*import { Redirect } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function Page() {
   return <Redirect href={"/(tabs)/study"} />;
-}*/
+}
