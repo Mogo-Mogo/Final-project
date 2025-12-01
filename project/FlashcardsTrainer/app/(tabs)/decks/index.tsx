d3ca6202db89ef9df114cc601be76970e1af0b60
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 21:30:36 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/(tabs)/decks/index.tsx
+++ project/FlashcardsTrainer/app/(tabs)/decks/index.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Page() {
   return (
     <View style={styles.container}>
-      <Text>this is the decks.index page</Text>
+      <Text>this is the decks.index webpage</Text>
     </View>
     
   );
