5ae2d0f48951ffc5c265bdcf6dbb441e6003114d
codespaces-autogit <codespaces-autogit@users.noreply.github.com>
2025-12-01 22:28:23 +0000

    --- Auto Save ---

--- project/FlashcardsTrainer/app/index.tsx
+++ project/FlashcardsTrainer/app/index.tsx
@@ -1,9 +1,10 @@
 //import { Redirect } from 'expo-router';
 import { View } from 'react-native';
 
+
 export default function Page() {
   return <View>
-    Hello
+    
   </View>
   //return <Redirect href={"/(tabs)/study"} />;
 }
